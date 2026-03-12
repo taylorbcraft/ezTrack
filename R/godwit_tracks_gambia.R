e51a028a025d0d63c6c7f9cb2cd6a391891d20ea
taylorbcraft <t.b.craft@rug.nl>
2026-03-12 10:17:10 +0900

    debug

--- R/godwit_tracks_gambia.R
+++ R/godwit_tracks_gambia.R
@@ -1,24 +1,19 @@
-#' Example Godwit Tracking Dataset from Gambia
+#' Example godwit tracking dataset from Gambia
 #'
 #' A dataset of godwit tracking data from Gambia used as an example for ezTrack functions.
 #'
 #' @format A data frame with columns:
 #' \describe{
-#'   \item{individual.local.identifier}{Unique identifier for each tracked animal}
+#'   \item{id}{Unique identifier for each tracked animal}
+#'   \item{ring_id}{Ring identifier}
+#'   \item{sensor}{Sensor type or device identifier}
+#'   \item{sex}{Sex of the individual}
 #'   \item{timestamp}{Datetime of the location fix}
-#'   \item{location.long}{Longitude coordinate (WGS84)}
-#'   \item{location.lat}{Latitude coordinate (WGS84)}
+#'   \item{x}{Longitude coordinate}
+#'   \item{y}{Latitude coordinate}
 #' }
-#' @source Movebank
-#'
 #' @docType data
 #' @keywords datasets
 #' @name godwit_tracks_gambia
 #' @usage data(godwit_tracks_gambia)
-#'
-#' @examples
-#' data(godwit_tracks_gambia)
-#' godwit_tracks_gambia <- ez_track(godwit_tracks_gambia)
-#' ez_summary(godwit_tracks_gambia)
-#' ez_home_range(godwit_tracks_gambia)
 "godwit_tracks_gambia"
